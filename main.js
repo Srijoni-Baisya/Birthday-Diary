62cd8fe6ea6dd39a1585ee4599e474c2bd22a97e
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2022-04-03 11:54:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
 //ADD YOUR FIREBASE LINKS HERE
 var firebaseConfig = {
     apiKey: "AIzaSyDvsUUJdOKEbrLRwp0tbi6LaGjtrJG3boI",
-
-    authDomain: "birthday-diary-d63cf.firebaseapp.com",
-  
-    databaseURL: "https://birthday-diary-d63cf-default-rtdb.firebaseio.com",
-  
-    projectId: "birthday-diary-d63cf",
-  
-    storageBucket: "birthday-diary-d63cf.appspot.com",
-  
-    messagingSenderId: "336218975090",
-  
-    appId: "1:336218975090:web:d0fb2a8f4d72a628700beb"
-  
+    authDomain: "birthday-diary-d63cf.firebaseapp.com",  
+    databaseURL: "https://birthday-diary-d63cf-default-rtdb.firebaseio.com",  
+    projectId: "birthday-diary-d63cf",  
+    storageBucket: "birthday-diary-d63cf.appspot.com",  
+    messagingSenderId: "336218975090",  
+    appId: "1:336218975090:web:d0fb2a8f4d72a628700beb"  
     };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -34,13 +27,15 @@ function show_details(){
      age= message_data['Current_age'];
      birthday= message_data['Birthday'];
      link = message_data['Photo'];
+     food = message_data['Food'];
 
      name_tag = "<h3 class = 'info'> Name : "+member+" </h3>";
      age_tag = "<h3 class = 'info'> Current Age : "+age+" </h3>";
      birthday_tag = "<h3 class = 'info'> Birthday : "+birthday+" </h3>";
-     img_tag = "<img class = 'pic' src = ' " + link + "'>"
+     food_tag = "<h3 class = 'info'> Favourite Food : "+food+" </h3>";
+     img_tag = "<img class = 'img-responsive pic' src = ' " + link + "'>"
      document.getElementById("details").style.display = "inline-block";
-     document.getElementById("details").innerHTML = name_tag + age_tag + birthday_tag + img_tag;;
+     document.getElementById("details").innerHTML = name_tag + age_tag + birthday_tag + food_tag + img_tag;;
 
     //End code
      } });  }); }
